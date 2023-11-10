24be95791f233e34d459eaf9edf998dd26b85e50
Erchis Patwardhan <erchispatwardhan@berkeley.edu>
2023-11-10 13:09:16 -0800

    Creating project structure

--- App.tsx
+++ App.tsx
@@ -1,22 +1,61 @@
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="" component={MapScreen} />
+        <Stack.Screen name = 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  container : {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  container: {
+    flex: 1
   },
-  text : {
-    fontSize: 16
+  buffer: {
+    flex: 1
+  },
+  buttonTabBar : {
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
+    padding: 15,
+    borderTopWidth: 2
+
+  },
+  button : {
+    fontSize: 20
   }
 });
+
 export default App;
