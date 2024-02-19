4df8e3f71118d8ee50d2d037f77b1566a741b47d
Erchis Patwardhan <erchispatwardhan@berkeley.edu>
2024-02-19 14:52:36 -0800

    Added parallax scroll

--- Screens/User/ProductScreen.jsx
+++ Screens/User/ProductScreen.jsx
@@ -34,36 +34,46 @@ const ProductScreen = ({route, navigation}) => {
   const [products, set_products] = useState([])
   const { shop } = route.params
   const [isLoading, setIsLoading] = useState(true);
+
   const scrollRef = useAnimatedRef();
   const scrollOffset = useScrollViewOffset(scrollRef);
-  const CustomHeader = ({ opacity }) => {
-    console.log(opacity)
-    return (
-      <View style={[styles.header, { opacity }]}>
-        <Text style={styles.headerText}>My Custom Header</Text>
-      </View>
-    );
-  };
 
+
+
+  useEffect(() => {
+    Promise.all([
+      Image.prefetch("https://reactjs.org/logo-og.png"),
+      get_product_for_shop(shop._id),
+
+    ]).then(([image_prefetch_result, data]) => {
+        set_products(data)
+        setIsLoading(false)
+      })
+  }, []);
 
   const imageAnimatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateY: interpolate(
-            scrollOffset.value,
-            [-IMG_HEIGHT, 0, IMG_HEIGHT],
-            [-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
-          )
-        },
-        {
-          scale: interpolate(scrollOffset.value, [-IMG_HEIGHT, 0, IMG_HEIGHT], [2, 1, 1])
-        }
-      ]
-    };
+
+      console.log("sldfjklsdjfkldsjfklsdjklfjsdklfjs")
+      return {
+        transform: [
+          {
+            translateY: interpolate(
+              scrollOffset.value,
+              [-IMG_HEIGHT, 0, IMG_HEIGHT],
+              [-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
+            )
+          },
+          {
+            scale: interpolate(scrollOffset.value, [-IMG_HEIGHT, 0, IMG_HEIGHT], [2, 1, 1])
+          }
+        ]
+      };
   });
 
   const headerAnimatedStyle = useAnimatedStyle(() => {
+    console.log({
+      opacity: interpolate(scrollOffset.value, [0, IMG_HEIGHT / 1.5], [0, 1])
+    })
     return {
       opacity: interpolate(scrollOffset.value, [0, IMG_HEIGHT / 1.5], [0, 1])
     };
@@ -72,7 +82,7 @@ const ProductScreen = ({route, navigation}) => {
   const Header = () => {
     const insets = useSafeAreaInsets();
     return (
-      <Animated.View style={ [headerAnimatedStyle, styles3.container, {paddingTop: insets.top}] }>
+      <Animated.View style={[headerAnimatedStyle, styles3.container, { paddingTop: insets.top }]}>
         <Text style={styles3.text}>Product Screen</Text>
       </Animated.View>
     )
@@ -81,43 +91,86 @@ const ProductScreen = ({route, navigation}) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTransparent: true,
-      headerLeft: () => <View/>,
+      headerLeft: () => <View />,
       headerTitle: '',
-      headerBackground: () => <Header/>
+      headerBackground: () => <Header />
     });
   }, [navigation]); // Re-run when navigation changes (though usually it doesn't)
 
 
-  useEffect(() => {
-    get_product_for_shop(shop._id)
-      .then((data) => {
+  if (isLoading) {
+    return (
+      <View style={{flex: 1 }}>
+        <View style={{backgroundColor: 'grey', height: 300, width: width}}/>
+        <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16} contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center'}}>
+          {/*<View style={styles.loadingContainer}/>*/}
 
-        set_products(data)
-        setIsLoading(false)
-      })
-  }, []);
+
+          {/*<Title text={"Products"} />*/}
+
+          <ActivityIndicator size="large" color="#8E0000"/>
+          <View style={{height: 50}}/>
+          {/* You can add additional text or graphics here */}
+        </Animated.ScrollView>
+        <TabBar navigation={navigation} />
+
+      </View>
+
+    );
+  } if (isLoading === false) {
+    const insets2 = useSafeAreaInsets();
+
+
+
+
 
     return (
+      <View flex={1}>
+        {/*<Title text={"Products"} />*/}
+        <View style={[styles.container, ]}>
 
-      <View style={styles.container}>
-        <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
-          <Animated.Image
-            source={{
-              uri: "https://reactjs.org/logo-og.png"
-            }}
-            style={[styles.image, imageAnimatedStyle]}
-          />
-          <View style={{ height: height, backgroundColor: '#fff' }}>
-            <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginTop: 20 }}>
-              Parallax Scroll
-            </Text>
+          <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
+            <Animated.Image
+              source={{
+                uri: "https://reactjs.org/logo-og.png"
+              }}
+              style={[styles.image, imageAnimatedStyle]}
+            />
+            <View style={{ height: 10000, backgroundColor: '#fff' }}>
+              <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginTop: 20 }}>
+                Parallax Scroll
+              </Text>
+                <FlatList
+                  scrollEnabled={false}
+                  keyExtractor={(item) => item._id.toString()}
+                  data={products}
+                  renderItem={({ item: product }) => (
+                    <ListProduct
+                      name={product.name}
+                      category={product.category}
+                      price={product.price}
+                      description={product.description}
+                      navigation={navigation}
+                      shop={shop}
+                      images={product.images}
+                      product={product}
+                    />
+                  )}
+                  contentContainerStyle={{ paddingBottom: 87 }}
+                />
+            </View>
 
-          </View>
-        </Animated.ScrollView>
+          </Animated.ScrollView>
+          <TabBar navigation={navigation} />
+
+
+        </View>
 
       </View>
     )
+  }
 }
+
 
 
 const styles = StyleSheet.create({
@@ -137,6 +190,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     position: 'absolute',
     top: 0,
+  },
+  loadingContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
 
@@ -156,4 +213,3 @@ const styles3 = StyleSheet.create({
 })
 
 export default ProductScreen;
-
